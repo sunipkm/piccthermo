ee9d7af6c2209b8e40cb68c0db7ba1afbbc81de0
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-06-06 14:05:22 -0400

    Original thermo is aarch64, detect build config. Overwrite thermo.env only if it does not exist

--- push.sh
+++ push.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-cross build --target armv7-unknown-linux-gnueabihf --release
+TARGET="armv7-unknown-linux-gnueabihf"
+REMOTE_TARGET=$(ssh pyro uname -m)
+if [ $? -eq 0 ]; then
+    if [ $REMOTE_TARGET = "aarch64" ]; then
+        echo "Remote system is ARM64."
+        TARGET="aarch64-unknown-linux-gnu"
+    fi
+fi
+cross build --target $TARGET --release
 ssh pyro -t 'mkdir -p ~/thermo-server'
-scp target/armv7-unknown-linux-gnueabihf/release/thermo-server pyro:~/thermo-server
-scp thermo.env thermo.service pyro:~/thermo-server
+scp target/$TARGET/release/thermo-server pyro:~/thermo-server
+ssh -q pyro -t 'stat "thermo-server/thermo.env" &> /dev/null'
+if [ $? -ne 0 ]; then
+    scp thermo.env thermo.service pyro:~/thermo-server
+fi
 scp disable_ethernet_gadget.sh pyro:~/thermo-server
