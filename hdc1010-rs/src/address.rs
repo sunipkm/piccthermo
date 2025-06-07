d5d913c43cefce38a8adb296cfa588f417e5d909
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-06-07 01:10:39 -0400

    Added the HDC1010 driver

---
+++ hdc1010-rs/src/address.rs
@@ -0,0 +1,15 @@
+use bitfield_struct::bitfield;
+
+#[bitfield(u8)]
+/// Represents the slave address for the HDC1010 sensor.
+/// The address is 7 bits long, with the least significant bit (LSB) used for read/write operations.
+/// The default address is 0x40, which is the standard I2C address for the HDC1010.
+/// The address can be configured by setting the `a0` and `a1` bits.
+pub struct SlaveAddress {
+    #[bits(1, default = false)]
+    pub a0: bool,
+    #[bits(1, default = false)]
+    pub a1: bool,
+    #[bits(6, default = 0x40)]
+    reserved: u8,
+}
